5ea4d19ba47f9ab59d6e814791c3809041790a23
Arthur Marques <arthurmduarte@proton.me>
2025-01-16 19:20:03 -0300

    Criação e configuração inicial de projeto.

---
+++ src/main/java/com/amd/desafio1/Desafio1SeaTecnologiaApplication.java
@@ -0,0 +1,13 @@
+package com.amd.desafio1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafio1SeaTecnologiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desafio1SeaTecnologiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amd/desafio1/Desafio1SeaTecnologiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amd.desafio1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desafio1SeaTecnologiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
