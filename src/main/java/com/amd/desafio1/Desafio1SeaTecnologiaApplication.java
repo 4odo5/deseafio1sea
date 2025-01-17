888dd99cc595f429cb1110ce8047b10c81555dcb
Arthur Marques <arthurmduarte@proton.me>
2025-01-17 00:36:05 -0300

    Configuração prévia do mapper

--- src/main/java/com/amd/desafio1/Desafio1SeaTecnologiaApplication.java
+++ src/main/java/com/amd/desafio1/Desafio1SeaTecnologiaApplication.java
@@ -1,13 +1,20 @@
 package com.amd.desafio1;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Desafio1SeaTecnologiaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Desafio1SeaTecnologiaApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper getModelMapper() {
+		return new ModelMapper();
 	}
 
 }
